558bc4b0d39ede7985aa289c62d788b4b96c5792
Amanda Robinson <amanda.robinson@jpl.nasa.gov>
2023-06-27 09:27:55 -0700

    Changed get_pds4_inputs.py to display wiki2vec tokenization & embeddings

---
+++ src/pds/llm/Wiki2Vec.py
@@ -0,0 +1,8 @@
+from wikipedia2vec import Wikipedia2Vec
+#Downloaded file from Wikipedia2vec/https://wikipedia2vec.github.io/wikipedia2vec/pretrained/
+MODEL_FILE = '/Users/arobinson/Documents/enwiki_20180420_100d.pkl'
+wiki2vec = Wikipedia2Vec.load(MODEL_FILE)
+word_vector = wiki2vec.get_word_vector('moons')
+similar_words = wiki2vec.most_similar(wiki2vec.get_word('jovian'), 20)
+print("Word vector for 'moons':", word_vector)
+print("Similar words to 'europa':", similar_words)
