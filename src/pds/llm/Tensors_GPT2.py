617fa93d016af2c82446b1a4b9796fac18591f7d
Amanda Robinson <amanda.robinson@jpl.nasa.gov>
2023-07-31 15:20:40 -0700

    unable to configure cosine similarity between PDS4 labels & search terms, attempted to use the cos/sim from Wiki2Vec

--- src/pds/llm/Tensors_GPT2.py
+++ src/pds/llm/Tensors_GPT2.py
@@ -6,11 +6,12 @@ import numpy as np
 from numpy.linalg import norm
 import statistics
 import pandas as pd
+from scipy.sparse import csr_matrix
 
-URLS = [
-    'https://atmos.nmsu.edu/PDS/data/PDS4/saturn_iono/data/rss_s10_r007_ne_e.xml',
-    'https://planetarydata.jpl.nasa.gov/img/data/nsyt/insight_cameras/data/sol/0024/mipl/edr/icc/C000M0024_598662821EDR_F0000_0558M2.xml'
-]
+URLS = {
+    "cassini": 'https://atmos.nmsu.edu/PDS/data/PDS4/saturn_iono/data/rss_s10_r007_ne_e.xml',
+    "insight": 'https://planetarydata.jpl.nasa.gov/img/data/nsyt/insight_cameras/data/sol/0024/mipl/edr/icc/C000M0024_598662821EDR_F0000_0558M2.xml'
+}
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')
 search_terms = [
@@ -35,37 +36,75 @@ def get_pds4_xml_data(url):
     xml_data = response.text
     return xml_data
 
-def tokenize_pds4_xml_files(xml_data):
-    xml_dict = xmltodict.parse(xml_data)
+def parse_xml_like_data(xml_data):
+    lines = xml_data.strip().split('\n')
+    data_dict = {}
+    for line in lines:
+        key_value = line.split(' ', 1)
+        if len(key_value) == 2:
+            key, value = key_value
+            try:
+                data_dict[key] = csr_matrix([float(value)])
+            except ValueError:
+                pass
+    return data_dict
+
+
+def get_tensors_for_pds_labels(data_dict):
+    xml_dict = xmltodict.parse(data_dict)
     xml_string = json.dumps(xml_dict)
     pds_tokens = tokenizer(xml_string, return_tensors='pt').input_ids
     print("PDS4 Label Tensors:")
     print(pds_tokens)
     return pds_tokens
 
-def tokenize_search_terms():
+
+def get_tensors_for_search_terms():
     search_term_tokens = [tokenizer(term, return_tensors='pt').input_ids for term in search_terms]
     print("Search Term Tensors:")
     for i, term in enumerate(search_terms):
         print(f"Tensor for '{term}':")
         print(search_term_tokens[i])
+        # Convert dense tensor to sparse tensor (CSR format)
+        search_term_tokens[i] = csr_matrix(search_term_tokens[i].numpy())
     return search_term_tokens
+
 
 def cosine_similarity(a, b):
     return np.dot(a, b) / (norm(a) * norm(b))
 
 def get_token_max_similarity(v_ref, vs):
     cos_sims = [cosine_similarity(v_ref, v) for v in vs.values()]
+    if not cos_sims:
+        return 0.0
     return max(cos_sims)
 
 
 def main():
     label_embeddings = {}
+    for label, url in URLS.items():
+        xml_data = get_pds4_xml_data(url)
+        data_dict = parse_xml_like_data(xml_data)
+        label_embeddings[label] = data_dict
 
+    search_embeddings = get_tensors_for_search_terms()
+
+    mean_max_sims = []
+    for term in search_terms:
+        line = dict(search_term=term)
+        for label, url in URLS.items():
+            max_similarities = []
+            for vector in search_embeddings:
+                max_similarity = get_token_max_similarity(vector, label_embeddings[label])
+                max_similarities.append(max_similarity)
+            line[label] = statistics.mean(max_similarities) if max_similarities else np.nan
+        mean_max_sims.append(line)
+
+    df = pd.DataFrame(mean_max_sims)
+    print(df)
 
 if __name__ == '__main__':
     main()
-
 
 '''
 #Prints embeddings using Pipeline
